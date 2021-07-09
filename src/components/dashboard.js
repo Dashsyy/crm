598b00f92908922870f649fdc9ac23db94b7fdbd
Sunhour <devsunhour@gmail.com>
2021-07-09 20:39:44 +0700

    First Init

---
+++ src/components/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function dashboard() {
+    return (
+        <div>
+            <h1> Customer Relations Management</h1>
+        </div>
+    )
+}
+
+export default dashboard
